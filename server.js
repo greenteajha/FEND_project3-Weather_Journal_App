c7bfa59ff8f348b07ae6050a306a9615e2cc43e4
Jeremy Teo <jeremyteo88@hotmail.com>
2020-12-11 13:46:05 +0800

    Created Node express server.

---
+++ server.js
@@ -0,0 +1,31 @@
+// Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+// Add dependencies
+// Add body-parser dependencies
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+// Add cors dependencies
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the "website" folder
+app.use(express.static('website'));
+
+// Indicate port for server to listen to
+const port = 8080;
+const server = app.listen(port, listening);
+
+function listening(){
+    console.log(`Server is up! Server is running on localhost: ${port}`);
+}
+
+app.get("/", function(req, res){
+    console.log(req);
+    res.send("Hello World");
+})
