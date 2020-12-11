eb62f5b5dbf77c939ae8eb9711cf735b37923f40
Jeremy Teo <jeremyteo88@hotmail.com>
2020-12-12 03:07:37 +0800

    Completed version 1 - Added CSS contents, dynamic card per weather journal entry.

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// API Endpoint
+const projectData = [];
+
 // Express to run server and routes
 const express = require('express');
 
@@ -22,27 +25,32 @@ const port = 8080;
 const server = app.listen(port, listening);
 
 function listening(){
-    console.log(`Server is up! Server is running on localhost: ${port}`);
+    console.log(`Server is up and running on localhost port ${port}!`);
 }
 
-// Endpoint
-const projectData = [];
-
 // GET Route
-app.get('/add', function(req, res){
+app.get('/all', returnData);
+
+// Return API endpoint data, all journal entries to the client
+function returnData(req, res){
+    console.log(projectData);
     res.send(projectData);
-})
+}
+
 
 // POST Route
 app.post('/addData', addTemperature);
 
+// Receive POST data and add it into the API endpoint
 function addTemperature(req,res){
 
     //console.log(req.body);
     newEntry = {
-        temperature: req.body.temperature
+        temperature: req.body.temperature,
+        entryDate: req.body.date,
+        feelings: req.body.feeling
     }
 
     projectData.push(newEntry);
-    console.log(projectData);
+    //console.log(projectData);
 }
--- website/js/app.js
+++ website/js/app.js
@@ -1,27 +1,76 @@
 // Indicate the API base URL and API key
-let baseURL = 'https://api.openweathermap.org/data/2.5/weather?q='
-let apiKey = '&appid=7e82b5b181bdba284b3d7b1ec57992d5';
+let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip='
+const apiKey = '&appid=7e82b5b181bdba284b3d7b1ec57992d5';
 
 // Create an event listener for clicking the button
-document.getElementById('generate').addEventListener('click', retrieveCity);
+document.getElementById('generate').addEventListener('click', retrieveZipCode);
 
-function retrieveCity(e){
-  const cityName =  document.getElementById('city').value;
-  getWeather(baseURL, cityName, apiKey)
+// Function to retrieve weather data through API
+function retrieveZipCode(e){
+  const zipCode = document.getElementById('zip').value;
+  const feelings = document.getElementById('feelings').value;
 
+// Chaining promise...
+  getWeather(baseURL, zipCode, apiKey)
   .then(function(data){
-    //console.log(data.main.temp);
-    postData('/addData', {temperature:data.main.temp});
-  });
+    //console.log(data.dt);
+    postData('/addData', {temperature:data.main.temp, date: data.dt, feeling:feelings});
+    updateUI();
+  })
 };
 
+// Async function to update the user interface after retrieving the data from the server
+const updateUI = async () => {
+  const request = await fetch('/all');
+  try{
+    const allData = await request.json();
+
+    // Update UI
+    let counter = 1;
+    const cardContainer = document.getElementById('entryHolder');
+    cardContainer.innerHTML = '';
+
+    for(const eachEntry of allData){
+      console.log(eachEntry);
+      console.log(counter);
+      let card = document.createElement('div');
+      card.setAttribute('id','card'+counter);
+      card.setAttribute('class','card');
+      let cardTemp = document.createElement('div');
+      cardTemp.setAttribute('id','temp'+counter);
+      cardTemp.setAttribute('class','temp');
+      cardTemp.innerHTML = (Math.round(eachEntry.temperature - 273)) + " &#x2103";
+      let cardContent = document.createElement('div');
+      cardContent.setAttribute('id','content'+counter);
+      cardContent.setAttribute('class','content');
+      cardContent.innerHTML = "I'm feeling: " + eachEntry.feelings;
+      let cardDate = document.createElement('div');
+      cardDate.setAttribute('id','date'+counter);
+      cardDate.setAttribute('class','date');
+      cardDate.innerHTML = new Date(eachEntry.entryDate * 1000).toDateString();
+      card.appendChild(cardDate);
+      card.appendChild(cardTemp);
+      card.appendChild(cardContent);
+      cardContainer.appendChild(card);
+      counter += 1;
+    }
+    //document.getElementById('temp').innerHTML = celciusTemp+" Degrees Celcius";
+    //document.getElementById('content').innerHTML = allData[0].feelings;
+    //document.getElementById('date').innerHTML = currentDate;
+
+  }catch(error){
+    console.log("error", error);
+  }
+}
+
 //Async function to request the API to return back the response based on the city name keyed in
-const getWeather = async (baseURL, cityName, key)=>{
+const getWeather = async (baseURL, zipCode, key)=>{
   
-  const res = await fetch(baseURL+cityName+key)
+  const res = await fetch(baseURL+zipCode+key)
 
   try {
     const data = await res.json();
+    //console.log(data);
     return data;    
   }catch(error) {
     console.log("error", error);
